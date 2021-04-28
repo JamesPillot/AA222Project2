6cab6e077c685fd042215de1783df011fcdc36d2
James P <38028854+JamesPillot@users.noreply.github.com>
2021-04-28 14:56:58 -0700

    Post problem 1-3 working
    This time with direct methods, first run through autograder for secret problems

--- project2_py/project2.py
+++ project2_py/project2.py
@@ -24,16 +24,16 @@ def gradient_descent(x, alpha, g):
     x_next = local_descent(x, alpha, d)
     return x_next
 
-def backtrack_line_search(f, g, x, d, alpha, p = .005, B = 1e-12):
+def backtrack_line_search_for_grad(f, g, x, d, alpha, p = .5, B = 1e-4):
     y = f(x)
     while(f(x + alpha*d) > y + B*alpha*(np.dot(g,d))):
         alpha *= p
     return alpha
 
-def gradient_descent_with_line_search(f, x, alpha, g):
-    g = g(x)
-    d = -g/np.linalg.norm(g)
-    alpha = backtrack_line_search(f, g, x, d, alpha)
+def gradient_descent_with_line_search(f, x, alpha, g, c_prime, pro):
+    gr = g(x)
+    d = -gr/np.linalg.norm(gr)
+    alpha = backtrack_line_search_for_grad(f, gr, x, d, alpha)
     x_next = local_descent(x, alpha, d)
     return x_next
 
@@ -45,17 +45,66 @@ def momentum(g, x, v, alpha, B):
     return x_next, v_next
 
 def nesterov_momentum(g, x, v, alpha, B, c_prime, pro):
-    gr = g(x + B*v) + pro*c_prime
+    gr = g(x + B*v)
     norm_gr = gr/np.linalg.norm(gr)
     v_next = B*v - alpha*norm_gr
     x_next = x + v_next
     return x_next, v_next
 
-def p_quadratic(c_eval, h_eval):
+def p_inv_barrier(x,c):
+    return -sum(1/c(x))
+
+def p_quadratic(x, c):
+    c_eval = c(x)
     zer_vec = np.zeros(len(c_eval))
     max_vec = np.maximum(c_eval, zer_vec)
-    p = (np.linalg.norm(max_vec)**2) + (np.linalg.norm(h_eval)**2)
+    p = (np.linalg.norm(max_vec)**2)
     return p
+
+def p_count(c_eval, h_eval):
+    p = np.sum((c_eval > 0)) 
+    return p
+
+def basis(i,n):
+    return [1.0 if k == i else 0.0 for k in range(n)]
+
+# def Powell_Method(f, x , powell_eps, alpha):
+#     n = len(x)
+#     U = np.array([basis(i,n) for i in range(n)])
+#     delt = np.inf
+#     while (delt > powell_eps):
+#         x_prime = x
+#         for i in range(n):
+#             d = U[i]
+#             x_prime = x + alpha*d
+#         for i in range((n-1)):
+#             U[i] = U[i+1]
+#         d = x_prime - x
+#         U[n-1] = d
+#         x_prime = x + alpha*d
+#         delt = np.linalg.norm((x_prime - x))
+#         x = x_prime
+#     return x
+
+def Hooke_Jeeves_penalty(f, c, p, pro, x, alpha, hooke_epsilon, gamma = .5):
+    y = f(x) + pro*p(x, c)
+    n = len(x)
+    while alpha > hooke_epsilon:
+        improved = False
+        x_best, y_best = x, y
+        for i in range(n):
+            for sgn in [-1, 1]:
+                x_prime = x + sgn*alpha*np.array(basis(i,n))
+                y_prime = f(x_prime) + pro*p(x_prime, c)
+                if(y_prime < y_best):
+                    x_best, y_best, improved = x_prime, y_prime, True
+        
+        x, y = x_best, y_best
+        if (not improved):
+            alpha *= gamma
+    
+    return x
+
 
 
     
@@ -79,67 +128,51 @@ def optimize(f, g, c, x0, n, count, prob):
     x_last = x0
     v_last = np.zeros(len(x0))
     pro = 1
+    gamma = 2
+    delta = np.inf
+    eps = .01
+    powell_eps = .1
+    hooke_eps = .1
 
     while(count() < n):
         if prob == "simple1":
-            # Hyperparameters
-            alpha = .1222
-            B = .55
-            gamma = 2
-            # evaluate constraints
-            c_eval = c(x_last)
-            # impose quadratic penalty 
-            p = p_quadratic(c_eval, 0)
-            if(p == 0):
-                return x_last
-            # Gradients and Jacobian of constraints for quadratic penalty
-            c1_grad = np.array([1, 2*x_last[1]])
-            c2_grad = np.array([-1, -1])
-            jac_c = np.array([c1_grad, c2_grad])
-            jac_c_t = np.transpose(jac_c)
-            c_eval = c(x_last)
-            zer_vec = np.zeros(len(c_eval))
-            c_prime = 2*np.sum(jac_c_t*np.maximum(c_eval, zer_vec))
-            # Minimize with nesterov penalty
-            x_next, v_next = nesterov_momentum(g, x_last, v_last, alpha, B, c_prime, pro)
-            # Update pro and x, v values for minimizer (nesterov)
-            pro *= gamma
-            x_last = x_next
-            v_last = v_next
+           alpha = .5
+           x_best = Hooke_Jeeves_penalty(f, c, p_quadratic, pro, x_last, alpha, hooke_eps)
+           pro *= gamma
+           if p_quadratic(x_best, c) == 0:
+               return x_best
+
         elif prob == "simple2":
-            # Hyperparameters
-            alpha = .13
-            B = 1.05 
-            gamma = 2
-            # evaluate constraints
-            c_eval = c(x_last)
-            # impose quadratic penalty 
-            p = p_quadratic(c_eval, 0)
-            if(p == 0):
-                return x_last
-            # Gradients and Jacobian of constraints for quadratic penalty
-            c1_grad = np.array([3*(x_last[0]-1)**2, -1])
-            c2_grad = np.array([1, 1])
-            jac_c = np.array([c1_grad, c2_grad])
-            jac_c_t = np.transpose(jac_c)
-            c_eval = c(x_last)
-            zer_vec = np.zeros(len(c_eval))
-            c_prime = 2*np.sum(jac_c_t*np.maximum(c_eval, zer_vec))
-            # Minimize with nesterov penalty
-            x_next, v_next = nesterov_momentum(g, x_last, v_last, alpha, B, c_prime, pro)
-            # Update pro and x, v values for minimizer (nesterov)
+            alpha = .5
+            x_best = Hooke_Jeeves_penalty(f, c, p_quadratic, pro, x_last, alpha, hooke_eps)
             pro *= gamma
-            x_last = x_next
-            v_last = v_next
+            if p_quadratic(x_best, c) == 0:
+                return x_best
+        elif prob == "simple3":
+            alpha = .5
+            gamma = 8
+            x_best = Hooke_Jeeves_penalty(f, c, p_quadratic, pro, x_last, alpha, hooke_eps)
+            pro *= gamma
+            if p_quadratic(x_best, c) == 0:
+                return x_best
+        
+        elif prob == "secret1":
+            alpha = .5
+            gamma = 8
+            x_best = Hooke_Jeeves_penalty(f, c, p_quadratic, pro, x_last, alpha, hooke_eps)
+            pro *= 8
+            if p_quadratic(x_best, c) == 0:
+                return x_best
+
+        elif prob == "secret2": 
+            alpha = .5
+            while(delta > eps):
+                x_best = Hooke_Jeeves_penalty(f, c, p_inv_barrier, 1/pro, x_last, alpha, hooke_epsilon)
+                delta = np.linalg.norm(x_best - x_last)
+                x_last = x_best
+                pro *= gamma
+            return x_best
         else:
             return float("nan")
-        # elif prob == "simple2": 
-        # elif prob == "simple3": 
-        # elif prob == "secret1":
-           
-        # elif prob == "secret2":       
-        
-
-    x_best = x_last
-
+            
     return x_best
